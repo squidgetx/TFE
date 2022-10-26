f3d905e866bb9e58662779b4f94949c4990fda98
Sylvan Zheng <squidgetx@gmail.com>
2022-10-26 13:25:09 -0400

    update

--- extension/src/js/twitter.js
+++ extension/src/js/twitter.js
@@ -1,283 +1,37 @@
+import { parseTweetHTML } from "./twitter_parser";
 import { BLACKLIST_ACCOUNTS } from "./accounts";
-const BLACKLIST_TEXTS = ["foxnews.com", "foxbusiness.com", "fox.com/news"];
 
 let checkEligibility = true;
 let lastObservedLength = 0;
 
-/*
- * Return HTML
- */
-let renderTweet = (tweetObj) => {
-  let element = document.createElement("div");
-  element.innerHTML = "<p>Hello World</p>";
-  element.classList.add("ext_tweet_container");
-  return element;
-};
-
-/*
- * Tweet filtering section
- */
-
-let parseAuthor = (tweet) => {
-  let links = Array.from(tweet.getElementsByTagName("a"));
-  let re = /^http(s?):\/\/twitter\.com\/([^\/]*)$/;
-  let matches = links
-    .map((a) => {
-      let m = a.href.match(re);
-      if (m && m.length > 1) {
-        return m[2];
-      }
-      return null;
-    })
-    .filter((a) => a != null);
-  if (matches) {
-    return matches[0];
-  }
-  return null;
-};
-
-let parseId = (tweet) => {
-  let links = Array.from(tweet.getElementsByTagName("a"));
-  let re = /^http(s?):\/\/twitter\.com\/([^\/]*)\/status\/([0-9]+)$/;
-  let matches = links
-    .map((a) => {
-      let m = a.href.match(re);
-      if (m && m.length > 2) {
-        return m[3];
-      }
-      return null;
-    })
-    .filter((a) => a != null);
-  if (matches) {
-    return matches[0];
-  }
-  return null;
-};
-
-let parseTime = (tweet) => {
-  let time = Array.from(tweet.getElementsByTagName("time"));
-  if (time.length > 0) {
-    return time[0].dateTime;
-  }
-  return null;
-};
-
-let parseText = (tweet) => {
-  let span = Array.from(tweet.getElementsByTagName("span"));
-  return [
-    ...new Set(
-      span
-        .filter((s) => s.childNodes.length == 1)
-        .map((s) => s.parentNode.textContent)
-    ),
-  ];
-};
-
-let checkText = function (text_arr) {
-  // Check if any of the text in this tweet should be blacklisted
-  for (const text of text_arr) {
-    if (text === undefined) {
+let filterTweets = function (tweets, treatment_group, logger) {
+  for (const tweet of tweets) {
+    if (tweet == null) {
       continue;
     }
-    for (const blacklist_text of BLACKLIST_TEXTS) {
-      if (text.toLowerCase().includes(blacklist_text)) {
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-// Extract text content from a node with arbitrary depth of children
-
-/*
- * arg: HTMLDOMElement
- * Returns an object containing pointers to the HTML elements
- * containing the major components of the tweet
- */
-let parseTweetHTML = function (node) {
-  const obj = {
-    nodes: {},
-    checks: {},
-  };
-  obj.nodes.text = node.querySelectorAll('[data-testid="tweetText"]')[0];
-  obj.nodes.userName = node.querySelectorAll('[data-testid="User-Names"]')[0];
-  obj.nodes.avatar = node.querySelectorAll(
-    '[data-testid="Tweet-User-Avatar"]'
-  )[0];
-  obj.nodes.cardWrapper = node.querySelectorAll(
-    '[data-testid="card.wrapper"]'
-  )[0];
-  obj.nodes.tweetPhoto = node.querySelector('[data-testid="tweetPhoto"]');
-  obj.nodes.retweet = node.querySelector('[data-testid="retweet"]');
-  obj.nodes.retweetCount = obj.nodes.retweet
-    ? obj.nodes.retweet.querySelector(
-        '[data-testid="app-text-transition-container"]'
-      )
-    : null;
-
-  obj.nodes.reply = node.querySelector('[data-testid="reply"]');
-  obj.nodes.replyCount = obj.nodes.reply
-    ? obj.nodes.reply.querySelector(
-        '[data-testid="app-text-transition-container"]'
-      )
-    : null;
-
-  obj.nodes.like = node.querySelector('[data-testid="like"]');
-  obj.nodes.likeCount = obj.nodes.like
-    ? obj.nodes.like.querySelector(
-        '[data-testid="app-text-transition-container"]'
-      )
-    : null;
-
-  obj.nodes.caret = node.querySelector('[area-label="More"]');
-  obj.nodes.share = node.querySelector('[aria-label="Share Tweet"]');
-  obj.nodes.socialContext = node.querySelector('[data-testid="socialContext"]');
-  obj.nodes.node = node;
-  obj.isThread = obj.nodes.avatar
-    ? obj.nodes.avatar.nextSibling !== null
-    : undefined;
-
-  // Check to see if there are other items embedded in the tweet
-  // Twitter doesn't provide a data-testid property for
-  // retweets that are text-only, for example
-  if (obj.nodes.text !== undefined) {
-    // Go up the DOM tree until there are two or more children
-    let parent = obj.nodes.text.parentNode;
-    while (true) {
-      if (!parent) {
-        obj.checks.embeddedMediaFound = false;
-        break;
-      }
-      const num_components = parent.childNodes.length;
-      if (num_components > 1) {
-        // we found the tweet content root node
-        // check that the text tweet is the first node
-        for (let i = 0; i < parent.childNodes.length; i++) {
-          let container = parent.childNodes[i];
-          if (container.contains(obj.nodes.text)) {
-            obj.nodes.tweetTextContainer = container;
-            if (i != 0) {
-              obj.nodes.preText = Array.from(parent.childNodes).slice(0, i);
-            }
-            break;
-          }
-        }
-        // find the tweet controls
-        let tweetControls = parent.lastChild;
-        if (!tweetControls.contains(obj.nodes.like)) {
-          // Sometimes there's another element here, usually a "promoted" tag
-          obj.nodes.footer = tweetControls;
-          tweetControls = tweetControls.previousSibling;
-          obj.nodes.embeddedMedia = Array.from(parent.childNodes).slice(
-            1,
-            num_components - 2
-          );
-        } else {
-          obj.nodes.embeddedMedia = Array.from(parent.childNodes).slice(
-            1,
-            num_components - 1
-          );
-        }
-        obj.nodes.tweetControls = tweetControls;
-        break;
-      }
-      parent = parent.parentNode;
-    }
-  }
-
-  // set whether the node is a reply
-  if (obj.nodes.preText) {
-    for (const pt of obj.nodes.preText) {
-      if (pt.innerText.includes("Replying to")) {
-        obj.isReply = true;
-      }
-    }
-  }
-
-  // get the social context "header"
-  obj.nodes.socialContextContainer = null;
-  if (obj.nodes.socialContext) {
-    let parent = obj.nodes.socialContext.parentNode;
-    while (true) {
-      if (!parent) {
-        break;
-      }
-      const num_components = parent.childNodes.length;
-      if (num_components > 1) {
-        if (
-          parent.firstChild.contains(obj.nodes.socialContext) &&
-          parent.lastChild.contains(obj.nodes.text)
-        ) {
-          obj.nodes.socialContextContainer = parent.firstChild;
-          break;
-        }
-      }
-      parent = parent.parentNode;
-    }
-  }
-
-  for (let key in obj.nodes) {
-    if (obj.nodes[key]) {
-      obj[key] = obj.nodes[key].innerText;
-    }
-  }
-  return obj;
-};
-
-let parseTweet = function (tweet) {
-  let tweet_obj = {
-    id: parseId(tweet),
-    author: parseAuthor(tweet),
-    text: parseText(tweet),
-    time: parseTime(tweet),
-  };
-  tweet_obj.blacklist_text = checkText(tweet_obj.text);
-  return tweet_obj;
-};
-
-let filterTweets = function (children, treatment_group, logger) {
-  for (const tweet of children) {
-    let tweet_obj = parseTweet(tweet);
     let labels = new Set();
     const AUTHOR = "author";
-    const LINK = "link";
     const SHOW = "show";
     const HIDE = "hide";
-    if (BLACKLIST_ACCOUNTS.includes(tweet_obj.author)) {
+    if (BLACKLIST_ACCOUNTS.includes(tweet.data.author)) {
       labels.add(AUTHOR);
-    }
-    if (tweet_obj.blacklist_text) {
-      labels.add(LINK);
     }
 
     let action = SHOW;
 
     // Value 0 is control group
-    if (treatment_group == 1) {
-      // Link-only blacklist
-      if (labels.has(LINK)) {
-        tweet.innerHTML = "";
-        action = HIDE;
-      }
-    } else if (treatment_group == 2) {
+    if (treatment_group != 0) {
       // Account-only blacklist
       if (labels.has(AUTHOR)) {
         action = HIDE;
-        tweet.innerHTML = "";
-      }
-    } else if (treatment_group == 3) {
-      // Both
-      if (labels.size > 0) {
-        tweet.innerHTML = "";
-        action = HIDE;
+        tweet.nodes.node.innerHTML = "";
       }
     }
 
-    tweet_obj.labels = labels;
+    tweet.data.labels = labels;
 
     if (labels.size > 0) {
-      console.log(tweet_obj);
+      console.log(tweet);
       if (checkEligibility) {
         chrome.storage.sync.set({ eligible: true }, () => {
           console.log("Eligibilty set to true");
@@ -286,7 +40,7 @@ let filterTweets = function (children, treatment_group, logger) {
       }
     }
 
-    logger.logEvent(tweet_obj, action);
+    logger.logEvent(tweet.data, action);
   }
 };
 
@@ -300,10 +54,12 @@ let transformTweet = function (obj) {
   obj.nodes.text.innerHTML =
     "<span>I'm baby artisan ennui helvetica put a bird on it air plant, street art thundercats health goth mumblecore bespoke normcore retro. Unicorn forage skateboard, biodiesel marfa meggings banjo fanny pack. Blue bottle single-origin coffee cred lo-fi, humblebrag praxis subway tile woke palo santo marfa mustache helvetica fixie paleo green juice. Four loko copper mug XOXO pug pickled leggings.</span>";
   //obj.nodes.tweetControls.remove();
-  if (obj.nodes.embeddedMedia) {
-    for (let i = 0; i < obj.nodes.embeddedMedia.length; i++) {
+  if (obj.nodes.embeddedMedia.length > 0) {
+    for (let i = 1; i < obj.nodes.embeddedMedia.length; i++) {
       obj.nodes.embeddedMedia[i].remove();
     }
+    obj.nodes.embeddedMedia[0].innerHTML =
+      "<div class='media'><img src='https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg' width=100% /></div>";
   }
   if (obj.nodes.footer) {
     obj.nodes.footer.remove();
@@ -318,9 +74,9 @@ let transformTweet = function (obj) {
   obj.nodes.replyCount.innerHTML = "<span class='metric'>42</span>";
   obj.nodes.retweetCount.innerHTML = "<span class='metric'>666</span>";
   obj.nodes.likeCount.innerHTML = "<span class='metric'>1337</span>";
-  obj.nodes.tweetControls.parentNode.replaceChild(
-    obj.nodes.tweetControls.cloneNode(true),
-    obj.nodes.tweetControls
+  obj.nodes.body.tweetControls.parentNode.replaceChild(
+    obj.nodes.body.tweetControls.cloneNode(true),
+    obj.nodes.body.tweetControls
   );
   obj.nodes.node.onclick = function () {
     return false;
@@ -355,12 +111,18 @@ let injectTweets = function (
   // Make sure every N tweets is an injected tweet
   const FREQ = 5;
   const OFFSET = 3;
-  for (let i = 0; i < feedParent.childNodes.length; i++) {
-    const node = feedParent.childNodes[i];
-    const obj = parseTweetHTML(node);
-    transformTweet(obj);
-    console.log(obj);
+  let prevNode = null;
+};
+
+const parseTweets = function (children) {
+  let prevNode = null;
+  const tweets = [];
+  for (let i = 0; i < children.length; i++) {
+    tweets.push(parseTweetHTML(children[i], prevNode));
+    prevNode = children[i];
   }
+  console.log(tweets);
+  return tweets;
 };
 
 let processFeed = function (document, observer, treatment_group, logger) {
@@ -381,8 +143,9 @@ let processFeed = function (document, observer, treatment_group, logger) {
     const children = parentNode.childNodes;
     if (children.length != lastObservedLength) {
       console.log("Timeline length changed: ", lastObservedLength);
-      filterTweets(children, treatment_group, logger);
-      injectTweets(parentNode, treatment_group, logger, lastObservedLength);
+      const tweets = parseTweets(children);
+      filterTweets(tweets, treatment_group, logger);
+      injectTweets(tweets, treatment_group, logger, lastObservedLength);
       lastObservedLength = children.length;
     }
     disableInjectedContextMenus();
---
+++ extension/src/js/twitter_parser.js
@@ -0,0 +1,310 @@
+/*
+ * This module exports parseTwitterHTML
+ */
+
+const parseId = function (tweet) {
+  let links = Array.from(tweet.getElementsByTagName("a"));
+  let re = /^http(s?):\/\/twitter\.com\/([^\/]*)\/status\/([0-9]+)$/;
+  let matches = links
+    .map((a) => {
+      let m = a.href.match(re);
+      if (m && m.length > 2) {
+        return m[3];
+      }
+      return null;
+    })
+    .filter((a) => a != null);
+  if (matches) {
+    return matches[0];
+  }
+  return null;
+};
+
+const parseTime = function (tweet) {
+  let time = Array.from(tweet.getElementsByTagName("time"));
+  if (time.length > 0) {
+    return time[0].dateTime;
+  }
+  return null;
+};
+
+const parseAuthor = function (tweet) {
+  let links = Array.from(tweet.getElementsByTagName("a"));
+  let re = /^http(s?):\/\/twitter\.com\/([^\/]*)$/;
+  let matches = links
+    .map((a) => {
+      let m = a.href.match(re);
+      if (m && m.length > 1) {
+        return m[2];
+      }
+      return null;
+    })
+    .filter((a) => a != null);
+  if (matches) {
+    return matches[0];
+  }
+  return null;
+};
+
+const findMeaningfulParent = function (root) {
+  if (!root) {
+    return root;
+  }
+  let node = root.parentNode;
+  while (true) {
+    if (node.childNodes.length > 1) {
+      // this is a branch point node
+      return node;
+    }
+    if (!node.parentNode) {
+      return node;
+    }
+    node = node.parentNode;
+  }
+};
+
+// Find the [great]*[grand]child of the given node
+// that does not contain a singular div or span as its child
+const findMeaningfulChild = function (root) {
+  if (!root) {
+    return root;
+  }
+  let node = root;
+  while (true) {
+    if (node.childNodes.length > 1) {
+      // this is a branch point node
+      return node;
+    }
+    if (node.childNodes.length == 0) {
+      // the last node was an empty div or span?
+      // return the parent node
+      return node.parentNode;
+    }
+    // we know there must be exactly one child now
+    const child = node.firstChild;
+    if (child.tagName == "DIV" || child.tagName == "SPAN") {
+      node = child;
+    } else {
+      return node;
+    }
+  }
+};
+
+const queryMeaningfulChild = function (node, selector) {
+  let result = node.querySelector(selector);
+  return findMeaningfulChild(result);
+};
+
+/* Look through the tweet object until we find the child that contains the usernames
+ * Then any elements after that object will be the "tweet footer" objects
+ */
+const parseTweetFooter = function (obj) {
+  let tweetFooter = [];
+  let found = false;
+  if (obj.nodes.tweet.childNodes.length > 1) {
+    for (let i = 0; i < obj.nodes.tweet.childNodes.length; i++) {
+      if (obj.nodes.tweet.childNodes[i].contains(obj.nodes.userName)) {
+        found = true;
+        continue;
+      }
+      if (found) {
+        tweetFooter.push(obj.nodes.tweet.childNodes[i]);
+      }
+    }
+  }
+  return tweetFooter;
+};
+
+/*
+ * parse the tweet body using the text node or photo node as the starting point
+ * to find the controls (like, retweet, share) container, any extra embedded media
+ * like retweets, pre-text (eg, "Replying To"),
+ * and post-control components (usually the "Promoted" tag)
+ */
+const parseTweetBody = function (obj) {
+  const body = {
+    preTextComponents: [],
+    text: obj.nodes.text,
+    embeddedMedia: [],
+    tweetControls: [],
+    postControlComponents: [],
+  };
+
+  let startingPoint = obj.nodes.text ? obj.nodes.text : obj.nodes.tweetPhoto;
+
+  if (startingPoint != undefined) {
+    const tweetBodyContainer = findMeaningfulParent(startingPoint);
+    const tweetComponents = tweetBodyContainer.childNodes;
+    // We assume that the tweet controls come after the text
+    // and interpret all in-between components as embedded media
+    // We also allow for an arbitrary number of components before and after
+
+    let components = body.preTextComponents;
+    for (let i = 0; i < tweetComponents.length; i++) {
+      if (tweetComponents[i].contains(obj.nodes.text)) {
+        components = body.embeddedMedia;
+        continue;
+      }
+      if (tweetComponents[i].contains(obj.nodes.retweet)) {
+        body.tweetControls = findMeaningfulChild(tweetComponents[i]);
+        components = body.postControlComponents;
+        continue;
+      }
+      components.push(findMeaningfulChild(tweetComponents[i]));
+    }
+  }
+  return body;
+};
+
+/* Get the higher level social context HTML container */
+const parseSocialContextContainer = function (obj) {
+  if (!obj.nodes.socialContext) {
+    return null;
+  }
+  let parent = obj.nodes.socialContext.parentNode;
+  while (true) {
+    if (!parent) {
+      return null;
+    }
+    const num_components = parent.childNodes.length;
+    if (num_components > 1) {
+      if (
+        parent.firstChild.contains(obj.nodes.socialContext) &&
+        parent.lastChild.contains(obj.nodes.text)
+      ) {
+        return parent.firstChild;
+      }
+    }
+    parent = parent.parentNode;
+  }
+};
+
+/* set whether the tweet is part of a thread
+ * collapsed thread means the thread is rolled up and the See More button is active
+ * isThread simply means whether the tweet is part of a thread at all
+ * You can usually tell because there's an extra HTML element next to the avatar
+ * except in the case when it's the last tweet in the thread
+ * in this case we need to look at the previous node to see whether that one is a thread or not
+ */
+const parseTweetThread = function (obj, prevNode) {
+  const thread = {
+    isThread: obj.nodes.threadMarker != null,
+    isCollapsedThread: false,
+  };
+
+  if (prevNode) {
+    if (prevNode.isThread && !prevNode.isCollapsedThread) {
+      thread.isThread = true;
+    }
+  }
+
+  if (obj.nodes.tweetFooter) {
+    thread.isCollapsedThread = obj.nodes.tweetFooter.some((a) =>
+      a.innerText.includes("Show this thread")
+    );
+  }
+  return thread;
+};
+
+const parseReply = function (obj) {
+  if (obj.nodes.preText) {
+    return obj.nodes.preText.some((a) => a.includes("Replying to"));
+  }
+  return false;
+};
+
+/*
+ * arg: HTMLDOMElement
+ * Returns an object containing pointers to the HTML elements
+ * containing the major components of the tweet
+ */
+export const parseTweetHTML = function (node, prevNode) {
+  const obj = {
+    nodes: {},
+    data: {},
+  };
+  obj.nodes.tweet = queryMeaningfulChild(node, '[data-testid="tweet"]');
+  if (!obj.nodes.tweet) {
+    return null;
+  }
+
+  obj.nodes.text = queryMeaningfulChild(node, '[data-testid="tweetText"]');
+  obj.nodes.userName = queryMeaningfulChild(node, '[data-testid="User-Names"]');
+  obj.nodes.avatar = queryMeaningfulChild(
+    node,
+    '[data-testid="Tweet-User-Avatar"]'
+  );
+  obj.nodes.threadMarker = node.querySelector(
+    '[data-testid="Tweet-User-Avatar"]'
+  ).nextSibling;
+  obj.nodes.cardWrapper = queryMeaningfulChild(
+    node,
+    '[data-testid="card.wrapper"]'
+  );
+  obj.nodes.tweetPhoto = queryMeaningfulChild(
+    node,
+    '[data-testid="tweetPhoto"]'
+  );
+  obj.nodes.retweet = queryMeaningfulChild(node, '[data-testid="retweet"]');
+  obj.nodes.retweetCount = obj.nodes.retweet
+    ? obj.nodes.retweet.querySelector(
+        '[data-testid="app-text-transition-container"]'
+      )
+    : null;
+
+  obj.nodes.reply = queryMeaningfulChild(node, '[data-testid="reply"]');
+  obj.nodes.replyCount = obj.nodes.reply
+    ? obj.nodes.reply.querySelector(
+        '[data-testid="app-text-transition-container"]'
+      )
+    : null;
+
+  obj.nodes.like = queryMeaningfulChild(node, '[data-testid="like"]');
+  obj.nodes.likeCount = obj.nodes.like
+    ? obj.nodes.like.querySelector(
+        '[data-testid="app-text-transition-container"]'
+      )
+    : null;
+
+  obj.nodes.caret = queryMeaningfulChild(node, '[area-label="More"]');
+  obj.nodes.share = queryMeaningfulChild(node, '[aria-label="Share Tweet"]');
+  obj.nodes.socialContext = queryMeaningfulChild(
+    node,
+    '[data-testid="socialContext"]'
+  );
+
+  obj.nodes.node = node;
+  obj.nodes.tweetFooter = parseTweetFooter(obj);
+  obj.nodes.body = parseTweetBody(obj);
+  obj.nodes.socialContextContainer = parseSocialContextContainer(obj);
+  obj.data.thread = parseTweetThread(obj, prevNode);
+  obj.data.isReply = parseReply(obj);
+  obj.data.id = parseId(obj.nodes.userName);
+  obj.data.author = parseAuthor(obj.nodes.userName);
+  obj.data.tweetTime = parseTime(obj.nodes.userName);
+  obj.data.isPromoted = obj.nodes.body.postControlComponents.some((a) =>
+    a.innerText.includes("Promoted")
+  );
+  obj.data.allText = node.innerText;
+  // tk link?
+
+  for (const key of [
+    "likeCount",
+    "replyCount",
+    "retweetCount",
+    "socialContext",
+    "text",
+  ]) {
+    if (obj.nodes[key]) {
+      obj.data[key] = obj.nodes[key].innerText;
+    }
+  }
+
+  for (const key of ["preTextComponents", "postControlComponents"]) {
+    if (obj.nodes.body[key]) {
+      obj.data[key] = obj.nodes.body[key].map((a) => a.innerText);
+    }
+  }
+
+  return obj;
+};
